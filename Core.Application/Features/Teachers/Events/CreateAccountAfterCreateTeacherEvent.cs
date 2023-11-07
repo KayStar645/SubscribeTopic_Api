cfd071a2f4d89068c830a910567c62c35e2f7e0b
THUAN\thuan <thuanpt182@gmail.com>
2023-11-08 02:40:14 +0700

    create account after create teacher

---
+++ Core.Application/Features/Teachers/Events/CreateAccountAfterCreateTeacherEvent.cs
@@ -0,0 +1,54 @@
+﻿using Core.Application.Contracts.Persistence;
+using Core.Application.Interfaces.Repositories;
+using Core.Domain.Entities;
+using Core.Domain.Entities.Identity;
+using MediatR;
+
+namespace Core.Application.Features.Teachers.Events
+{
+    public class CreateAccountAfterCreateTeacherEvent : INotification
+    {
+        public Teacher _teacher { get; set; }
+        public IUserRepository _userRepository { get; set; }
+
+        public IUnitOfWork _unitOfWork { get; set; }
+
+        public CreateAccountAfterCreateTeacherEvent(Teacher teacher, IUnitOfWork unitOfWork, IUserRepository userRepository)
+        {
+            _teacher = teacher;
+            _userRepository = userRepository;
+            _unitOfWork = unitOfWork;
+        }
+    }
+
+    public class CreateAccountAfterCreateTeacherHandler : INotificationHandler<CreateAccountAfterCreateTeacherEvent>
+    {
+        public async Task Handle(CreateAccountAfterCreateTeacherEvent pEvent, CancellationToken cancellationToken)
+        {
+            await Task.Yield();
+
+            try
+            {
+                User user = new User()
+                {
+                    UserName = pEvent._teacher.InternalCode,
+                    Password = pEvent._teacher.InternalCode,
+                };
+
+                await pEvent._userRepository.CreateAsync(user);
+
+                var userFind = await pEvent._userRepository.FindByNameAsync(user.UserName);
+                var teacher = pEvent._teacher;
+                teacher.UserId = userFind.Id;
+
+                await pEvent._unitOfWork.Repository<Teacher>().UpdateAsync(teacher);
+                await pEvent._unitOfWork.Save(cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex);
+            }
+        }
+
+    }
+}
