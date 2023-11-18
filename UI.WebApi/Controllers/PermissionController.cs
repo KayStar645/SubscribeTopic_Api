b3f853e307f1c73ebba1a5cafc37929789652d33
THUAN\thuan <thuanpt182@gmail.com>
2023-11-18 17:09:08 +0700

    close authe view permission

--- UI.WebApi/Controllers/PermissionController.cs
+++ UI.WebApi/Controllers/PermissionController.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Application.Interfaces.Identity;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Reflection;
 using UI.WebApi.Middleware;
@@ -7,6 +8,7 @@ namespace UI.WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class PermissionController : ControllerBase
     {
         private readonly IPermissionService _permissionService;
@@ -17,7 +19,6 @@ namespace UI.WebApi.Controllers
         }
 
         [HttpGet]
-        [Permission("Permission.View")]
         public async Task<ActionResult<List<string>>> Get()
         {
             var response = await _permissionService.GetList(Assembly.GetExecutingAssembly());
