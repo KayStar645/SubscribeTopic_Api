3aac1d26237780d7120b688cf0edb69f8185fffe
THUAN\thuan <thuanpt182@gmail.com>
2023-11-19 22:07:00 +0700

    registration sieve

---
+++ Core.Domain/DomainServicesRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Sieve.Services.Interface;
+using Sieve.Services;
+
+namespace Core.Domain
+{
+    public static class DomainServicesRegistration
+    {
+        public static IServiceCollection ConfigureDomainServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<ISieveProcessor, ThesisSieveProcessor>();
+
+            return services;
+        }
+    }
+}
---
+++ Core.Domain/Services/ThesisSieveProcessor.cs
@@ -0,0 +1,26 @@
+﻿using Core.Domain.Entities;
+using Microsoft.Extensions.Options;
+using Sieve.Models;
+using Sieve.Services.Interface;
+
+namespace Sieve.Services
+{
+    public class ThesisSieveProcessor : SieveProcessor
+    {
+        public ThesisSieveProcessor(
+            IOptions<SieveOptions> options,
+            ISieveCustomSortMethods customSortMethods,
+            ISieveCustomFilterMethods customFilterMethods)
+            : base(options, customSortMethods, customFilterMethods)
+        {
+        }
+
+        protected override SievePropertyMapper MapProperties(SievePropertyMapper mapper)
+        {
+            mapper.Property<Thesis>(p => p.LecturerThesis.Name)
+                .CanFilter();
+
+            return mapper;
+        }
+    }
+}
