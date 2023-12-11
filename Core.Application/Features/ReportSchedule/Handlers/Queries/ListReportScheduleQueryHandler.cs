cb9e91396c1008be6704d54fc1f03b4e4ab6ad84
THUAN\thuan <thuanpt182@gmail.com>
2023-12-11 07:23:06 +0700

    report schedule

--- Core.Application/Features/ReportSchedule/Handlers/Queries/ListReportScheduleQueryHandler.cs
+++ Core.Application/Features/ReportSchedule/Handlers/Queries/ListReportScheduleQueryHandler.cs
@@ -1,6 +1,126 @@
-﻿namespace Core.Application.Features.ReportSchedule.Handlers.Queries
+﻿using AutoMapper;
+using Core.Application.Constants;
+using Core.Application.Contracts.Persistence;
+using Core.Application.DTOs.ReportSchedule;
+using Core.Application.DTOs.Thesis;
+using Core.Application.Exceptions;
+using Core.Application.Features.ReportSchedule.Requests.Queries;
+using Core.Application.Responses;
+using Core.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Sieve.Models;
+using Sieve.Services.Interface;
+using System.Net;
+using System.Net.WebSockets;
+using ReportScheduleEntity = Core.Domain.Entities.ReportSchedule;
+
+namespace Core.Application.Features.ReportSchedule.Handlers.Queries
 {
-    public class ListReportScheduleQueryHandler
+    public class ListReportScheduleQueryHandler : IRequestHandler<ListReportScheduleRequest, PaginatedResult<List<ReportScheduleDto>>>
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly ISieveProcessor _sieveProcessor;
+        public IHttpContextAccessor _httpContextAccessor;
+
+        public ListReportScheduleQueryHandler(IUnitOfWork unitOfWork, IMapper mapper, 
+            ISieveProcessor sieveProcessor, IHttpContextAccessor httpContextAccessor)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _sieveProcessor = sieveProcessor;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<PaginatedResult<List<ReportScheduleDto>>> Handle(ListReportScheduleRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var validator = new ListReportScheduleDtoValidator(_unitOfWork);
+                var result = await validator.ValidateAsync(request);
+
+                if (result.IsValid == false)
+                {
+                    var errorMessages = result.Errors.Select(x => x.ErrorMessage).ToList();
+                    return PaginatedResult<List<ReportScheduleDto>>
+                        .Failure((int)HttpStatusCode.BadRequest, errorMessages);
+                }
+
+                var sieve = _mapper.Map<SieveModel>(request);
+
+                var query = _unitOfWork.Repository<ReportScheduleEntity>().GetAllInclude();
+
+                var userId = _httpContextAccessor.HttpContext.User.FindFirst(CONSTANT_CLAIM_TYPES.Uid)?.Value;
+                var userType = _httpContextAccessor.HttpContext.User.FindFirst(CONSTANT_CLAIM_TYPES.Type)?.Value;
+
+                if (userType == CLAIMS_VALUES.TYPE_TEACHER)
+                {
+                    // Lấy danh sách lịch của giảng viên
+                    var teacher = await _unitOfWork.Repository<Teacher>()
+                        .FirstOrDefaultAsync(x => x.UserId == int.Parse(userId));
+                    var reportSchedule = await _unitOfWork.Repository<ReportScheduleEntity>()
+                                                .Query()
+                                                .Where(x => x.Thesis.ThesisInstructions
+                                                    .Any(x => x.TeacherId == teacher.Id) ||
+                                                            x.Thesis.ThesisReviews
+                                                     .Any(x => x.TeacherId == teacher.Id))
+                                                .ToListAsync();
+                }
+                else if (userType == CLAIMS_VALUES.TYPE_STUDENT)
+                {
+                    // Lấy danh sách lịch của sinh viên
+                    var student = await _unitOfWork.Repository<Student>()
+                                .FirstOrDefaultAsync(x => x.UserId == int.Parse(userId));
+                    var reportSchedule = await _unitOfWork.Repository<ReportScheduleEntity>()
+                                                .Query()
+                                                .Where(x => x.Thesis.ThesisRegistration.Group.Members
+                                                    .Any(x => x.Student.UserId == int.Parse(userId)))
+                                                .ToListAsync();
+                }
+                else
+                {
+                    throw new UnauthorizedException(StatusCodes.Status403Forbidden);
+                }
+
+                if (request.isAllDetail || request.isGetThesis == true)
+                {
+                    query = query.Include(x => x.Thesis);
+                }
+                if (request.isAllDetail || request.isGetTeacher == true)
+                {
+                    query = query.Include(x => x.Teacher);
+                }
+
+                query = _sieveProcessor.Apply(sieve, query);
+
+                int totalCount = await query.CountAsync();
+
+                var reportSchedules = await query.ToListAsync();
+
+                var mapReportSchedules = _mapper.Map<List<ReportScheduleDto>>(reportSchedules);
+
+                return PaginatedResult<List<ReportScheduleDto>>.Success(
+                    mapReportSchedules, totalCount, request.page,
+                    request.pageSize);
+            }
+            catch (NotFoundException ex)
+            {
+                return PaginatedResult<List<ReportScheduleDto>>.Failure((int)HttpStatusCode.NotFound, new List<string> { ex.Message });
+            }
+            catch (BadRequestException ex)
+            {
+                return PaginatedResult<List<ReportScheduleDto>>.Failure((int)HttpStatusCode.BadRequest, new List<string> { ex.Message });
+            }
+            catch (UnauthorizedException ex)
+            {
+                return PaginatedResult<List<ReportScheduleDto>>.Failure(ex.ErrorCode, new List<string> { ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return PaginatedResult<List<ReportScheduleDto>>.Failure((int)HttpStatusCode.InternalServerError, new List<string> { ex.Message });
+            }
+        }
     }
 }
---
+++ UI.WebApi/Controllers/ReportScheduleController.cs
@@ -0,0 +1,144 @@
+﻿using Core.Application.DTOs.ReportSchedule;
+using Core.Application.Exceptions;
+using Core.Application.Features.Base.Requests.Commands;
+using Core.Application.Features.ReportSchedule.Requests.Commands;
+using Core.Application.Features.ReportSchedule.Requests.Queries;
+using Core.Application.Responses;
+using Core.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Net;
+using UI.WebApi.Middleware;
+
+namespace UI.WebApi.Controllers
+{
+    [Route("api/reportSchedule")]
+    [ApiController]
+    public class ReportScheduleController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ReportScheduleController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Lấy danh sách lịch theo vai trò của người đăng nhập (GV/SV)
+        /// </summary>
+        /// <remarks>
+        /// Ràng buộc: 
+        /// </remarks>
+        [HttpGet]
+        [Permission("ReportSchedule.View")]
+        public async Task<ActionResult> Get([FromQuery] ListReportScheduleRequest request)
+        {
+            var response = await _mediator.Send(request);
+
+            return StatusCode(response.Code, response);
+        }
+
+        /// <summary>
+        /// Chi tiết một 1 lịch
+        /// </summary>
+        /// <remarks>
+        /// Ràng buộc: 
+        /// - Id: int, required
+        /// </remarks>
+        [HttpGet("detail")]
+        [Permission("ReportSchedule.View")]
+        public async Task<ActionResult> Get([FromQuery] DetailReportScheduleRequest request)
+        {
+            var response = await _mediator.Send(request);
+
+            return StatusCode(response.Code, response);
+        }
+
+        /// <summary>
+        /// Thêm lịch
+        /// </summary>
+        /// <remarks>
+        /// Ràng buộc: 
+        /// - ThesisId: Lịch của đề tài nào
+        /// - DateTime: lớn hơn hiện tại
+        /// - Location: 500
+        /// - Type: W/R - Lịch hằng tuần/Lịch phản biện
+        /// </remarks>
+        [HttpPost]
+        [Permission("ReportSchedule.Create")]
+        public async Task<ActionResult> Post([FromBody] CreateReportScheduleDto request)
+        {
+            var command = new CreateReportScheduleRequest { createReportScheduleDto = request };
+            var response = await _mediator.Send(command);
+
+            var settings = new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                NullValueHandling = NullValueHandling.Include,
+                ContractResolver = new DefaultContractResolver
+                {
+                    NamingStrategy = new CamelCaseNamingStrategy()
+                }
+            };
+
+            var json = JsonConvert.SerializeObject(response, settings);
+
+            return StatusCode(response.Code, json);
+        }
+
+        /// <summary>
+        /// Sửa lịch
+        /// </summary>
+        /// <remarks>
+        /// Ràng buộc: 
+        /// - DateTime: lớn hơn hiện tại
+        /// - Location: 500
+        /// - Type: W/R - Lịch hằng tuần/Lịch phản biện
+        /// </remarks>
+        [HttpPut]
+        [Permission("ReportSchedule.Update")]
+        public async Task<ActionResult> Put([FromBody] UpdateReportScheduleDto request)
+        {
+            var command = new UpdateReportScheduleRequest { updateReportScheduleDto = request };
+            var response = await _mediator.Send(command);
+
+            return StatusCode(response.Code, response);
+        }
+
+        /// <summary>
+        /// Xóa lịch
+        /// </summary>
+        /// <remarks>
+        /// Ràng buộc: 
+        /// - Id: int, required
+        /// </remarks>
+        [HttpDelete]
+        [Permission("ReportSchedule.Delete")]
+        public async Task<ActionResult> Delete([FromQuery] DeleteBaseRequest<ReportSchedule> request)
+        {
+            try
+            {
+                var response = await _mediator.Send(request);
+                return StatusCode((int)HttpStatusCode.NoContent);
+            }
+            catch (NotFoundException ex)
+            {
+                var responses = Result<ReportScheduleDto>.Failure(ex.Message, (int)HttpStatusCode.NotFound);
+                return StatusCode(responses.Code, responses);
+            }
+            catch (BadRequestException ex)
+            {
+                var responses = Result<ReportScheduleDto>.Failure(ex.Message, (int)HttpStatusCode.BadRequest);
+                return StatusCode(responses.Code, responses);
+            }
+            catch (Exception ex)
+            {
+                var responses = Result<ReportScheduleDto>.Failure(ex.Message, (int)HttpStatusCode.InternalServerError);
+                return StatusCode(responses.Code, responses);
+            }
+        }
+    }
+}
