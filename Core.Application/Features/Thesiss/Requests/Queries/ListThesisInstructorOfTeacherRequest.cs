9e85eb2428dfe672c7718449c55a42ba3bf786fd
THUAN\thuan <thuanpt182@gmail.com>
2023-12-20 02:40:34 +0700

    STA-73: filter periodId

--- Core.Application/Features/Thesiss/Requests/Queries/ListThesisInstructorOfTeacherRequest.cs
+++ Core.Application/Features/Thesiss/Requests/Queries/ListThesisInstructorOfTeacherRequest.cs
@@ -1,5 +1,10 @@
-﻿using Core.Application.DTOs.Thesis;
+﻿using Core.Application.Contracts.Persistence;
+using Core.Application.DTOs.Common.Validators;
+using Core.Application.DTOs.Thesis;
 using Core.Application.Features.Base.Requests.Queries;
+using Core.Application.Transform;
+using FluentValidation;
+using PeriodEntity = Core.Domain.Entities.RegistrationPeriod;
 
 namespace Core.Application.Features.Thesiss.Requests.Queries
 {
@@ -14,5 +19,28 @@ namespace Core.Application.Features.Thesiss.Requests.Queries
         public bool? isGetThesisMajors { get; set; }
 
         public bool? isGetAll { get; set; }
+
+        public int? periodId { get; set; }
+    }
+
+    public class ListThesisInstructorOfTeacherValidator : AbstractValidator<ListThesisInstructorOfTeacherRequest>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ListThesisInstructorOfTeacherValidator(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+
+            Include(new ListBaseRequestValidator<ThesisDto>());
+
+            RuleFor(x => x.periodId)
+                .MustAsync(async (periodId, token) =>
+                {
+                    var exists = await _unitOfWork.Repository<PeriodEntity>()
+                        .FirstOrDefaultAsync(x => x.Id == periodId);
+                    return exists != null;
+                })
+                .WithMessage(id => ValidatorTransform.MustIn("periodId"));
+        }
     }
 }
