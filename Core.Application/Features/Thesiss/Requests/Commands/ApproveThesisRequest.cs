e71734bfa9ca864a5286a482c8e35bc34ccf8de2
THUAN\thuan <thuanpt182@gmail.com>
2023-12-20 01:04:33 +0700

    add approve thesis

---
+++ Core.Application/Features/Thesiss/Requests/Commands/ApproveThesisRequest.cs
@@ -0,0 +1,12 @@
+﻿using Core.Application.DTOs.Thesis;
+using Core.Application.DTOs.Thesis.Validators;
+using Core.Application.Responses;
+using MediatR;
+
+namespace Core.Application.Features.Thesiss.Requests.Commands
+{
+    public class ApproveThesisRequest : IRequest<Result<ThesisDto>>
+    {
+        public ApproveThesisDto? approveThesisDto { get; set; }
+    }
+}
