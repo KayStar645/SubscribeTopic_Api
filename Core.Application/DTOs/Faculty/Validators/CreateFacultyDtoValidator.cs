bef3af275b87e69ac5a22f79ed8b6780c24c7261
THUAN\thuan <thuanpt182@gmail.com>
2023-12-19 23:17:10 +0700

    fix validator unique internalCode & name faculty

--- Core.Application/DTOs/Faculty/Validators/CreateFacultyDtoValidator.cs
+++ Core.Application/DTOs/Faculty/Validators/CreateFacultyDtoValidator.cs
@@ -1,5 +1,7 @@
 ﻿using Core.Application.Contracts.Persistence;
+using Core.Application.Transform;
 using FluentValidation;
+using FacultyEntity = Core.Domain.Entities.Faculties;
 
 namespace Core.Application.DTOs.Faculty.Validators
 {
@@ -12,6 +14,26 @@ namespace Core.Application.DTOs.Faculty.Validators
             _unitOfWork = unitOfWork;
 
             Include(new FacultyDtoValidator(_unitOfWork));
+
+            RuleFor(x => x.InternalCode)
+                .NotEmpty().WithMessage(ValidatorTransform.Required("internalCode"))
+                .MaximumLength(50).WithMessage(ValidatorTransform.MaximumLength("internalCode", 50))
+                .MustAsync(async (internalCode, token) =>
+                {
+                    var exists = await _unitOfWork.Repository<FacultyEntity>()
+                                        .FirstOrDefaultAsync(x => x.InternalCode == internalCode);
+                    return exists == null;
+                }).WithMessage(ValidatorTransform.Exists("internalCode"));
+
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage(ValidatorTransform.Required("name"))
+                .MaximumLength(190).WithMessage(ValidatorTransform.MaximumLength("name", 190))
+                .MustAsync(async (name, token) =>
+                {
+                    var exists = await _unitOfWork.Repository<FacultyEntity>()
+                                        .FirstOrDefaultAsync(x => x.Name == name);
+                    return exists == null;
+                }).WithMessage(ValidatorTransform.Exists("name"));
         }
     }
 }
--- Core.Application/DTOs/Faculty/Validators/FacultyDtoValidator.cs
+++ Core.Application/DTOs/Faculty/Validators/FacultyDtoValidator.cs
@@ -2,7 +2,6 @@
 using Core.Application.Services;
 using Core.Application.Transform;
 using FluentValidation;
-using FacultyEntity = Core.Domain.Entities.Faculties;
 
 namespace Core.Application.DTOs.Faculty.Validators
 {
@@ -13,26 +12,6 @@ namespace Core.Application.DTOs.Faculty.Validators
         public FacultyDtoValidator(IUnitOfWork _unitOfWork)
         {
             unitOfWork = _unitOfWork;
-
-            RuleFor(x => x.InternalCode)
-                .NotEmpty().WithMessage(ValidatorTransform.Required("internalCode"))
-                .MaximumLength(50).WithMessage(ValidatorTransform.MaximumLength("internalCode", 50))
-                .MustAsync(async (internalCode, token) =>
-                {
-                    var exists = await _unitOfWork.Repository<FacultyEntity>()
-                                        .FirstOrDefaultAsync(x => x.InternalCode == internalCode);
-                    return exists == null;
-                }).WithMessage(ValidatorTransform.Exists("internalCode"));
-
-            RuleFor(x => x.Name)
-                .NotEmpty().WithMessage(ValidatorTransform.Required("name"))
-                .MaximumLength(190).WithMessage(ValidatorTransform.MaximumLength("name", 190))
-                .MustAsync(async (name, token) =>
-                {
-                    var exists = await _unitOfWork.Repository<FacultyEntity>()
-                                        .FirstOrDefaultAsync(x => x.Name == name);
-                    return exists == null;
-                }).WithMessage(ValidatorTransform.Exists("name"));
 
             RuleFor(x => x.PhoneNumber)
                 .Must(phoneNumber => string.IsNullOrEmpty(phoneNumber) || phoneNumber.Length == 10)
