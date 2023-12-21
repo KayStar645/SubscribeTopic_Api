99e30fbc61a805a5bfb87ae33a56b360e8d52c4a
THUAN\thuan <thuanpt182@gmail.com>
2023-12-21 22:24:08 +0700

    list thesis possible council

---
+++ Core.Application/Features/Thesiss/Requests/Queries/ListThesisPossibleCouncilRequest.cs
@@ -0,0 +1,10 @@
+﻿using Core.Application.DTOs.Thesis;
+using Core.Application.Features.Base.Requests.Queries;
+
+namespace Core.Application.Features.Thesiss.Requests.Queries
+{
+    public class ListThesisPossibleCouncilRequest : ListBaseRequest<ThesisDto>
+    {
+
+    }
+}
